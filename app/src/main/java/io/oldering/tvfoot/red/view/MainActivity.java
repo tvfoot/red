a32b44da1c0fd7631f32738e0c62b07d8b52b403
benoit <oldering@gmail.com>
2016-10-21 15:02:17 +0900

    firebase analytics for fun

--- app/src/main/java/io/oldering/tvfoot/red/view/MainActivity.java
+++ app/src/main/java/io/oldering/tvfoot/red/view/MainActivity.java
@@ -3,6 +3,8 @@ package io.oldering.tvfoot.red.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 import io.oldering.tvfoot.red.flowcontroller.FlowController;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.DESTINATION, "Mars");
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle);
+
         FlowController.launchMatchListActivity(this);
         finish();
     }
