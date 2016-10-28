568b44d3ba88a387ec86876fd5d9fb37619b415a
benoit <oldering@gmail.com>
2016-10-28 18:51:40 +0900

    come on, we got to get some tests running on Android man

--- app/src/androidTest/java/io/oldering/tvfoot/red/DataBindingAdaptersTest.java
+++ app/src/androidTest/java/io/oldering/tvfoot/red/DataBindingAdaptersTest.java
@@ -1,13 +1,18 @@
 package io.oldering.tvfoot.red;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
+import android.widget.ImageView;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
 public class DataBindingAdaptersTest {
@@ -25,23 +30,25 @@ public class DataBindingAdaptersTest {
         verify(view).setVisibility(View.VISIBLE);
     }
 
-    // @Test
-    // public void setImageResource() {
-    //     String resourceName = "b1";
-    //     String packageName = "packageName";
-    //
-    //     Resources resources = mock(Resources.class);
-    //     when(resources.getIdentifier(resourceName, "drawable", packageName)).thenReturn(R.drawable.b1);
-    //
-    //     Context context = mock(Context.class);
-    //     when(context.getResources()).thenReturn(resources);
-    //     when(context.getPackageName()).thenReturn(packageName);
-    //
-    //     ImageView imageView = mock(ImageView.class);
-    //     // TODO getContext is final so cannot be overriden...
-    //     when(imageView.getContext()).thenReturn(context);
-    //
-    //     DataBindingAdapters.setImageResource(imageView, resourceName);
-    //     verify(imageView).setImageResource(R.drawable.b1);
-    // }
+    // Multie dex files define...
+    @Ignore
+    @Test
+    public void setImageResource() {
+        String resourceName = "b1";
+        String packageName = "packageName";
+
+        Resources resources = mock(Resources.class);
+        when(resources.getIdentifier(resourceName, "drawable", packageName)).thenReturn(R.drawable.b1);
+
+        Context context = mock(Context.class);
+        when(context.getResources()).thenReturn(resources);
+        when(context.getPackageName()).thenReturn(packageName);
+
+        ImageView imageView = mock(ImageView.class);
+        // TODO getContext is final so cannot be overriden...
+        when(imageView.getContext()).thenReturn(context);
+
+        DataBindingAdapters.setImageResource(imageView, resourceName);
+        verify(imageView).setImageResource(R.drawable.b1);
+    }
 }
