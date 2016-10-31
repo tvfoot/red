17c90fc7fa2a34f92912ef57391910c5c9c0aca0
benoit <oldering@gmail.com>
2016-10-31 22:18:26 +0900

    match detail activity squeleton

---
+++ app/src/main/java/io/oldering/tvfoot/red/view/MatchDetailActivity.java
@@ -0,0 +1,15 @@
+package io.oldering.tvfoot.red.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.oldering.tvfoot.red.R;
+
+public class MatchDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_detail);
+    }
+}
