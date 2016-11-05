17cc90672fa8197161c12d6d6f0b72a90eab8d06
benoit <oldering@gmail.com>
2016-11-05 16:25:53 +0900

    using gradle-play-publisher and download service-account

---
+++ misc/download_service-account.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# use curl to download the service-account.json from $SERVICE_ACCOUNT_URI, if set,
+# to the path/filename set in $SERVICE_ACCOUNT.
+if [[ $SERVICE_ACCOUNT && ${SERVICE_ACCOUNT} && $SERVICE_ACCOUNT_URI && ${SERVICE_ACCOUNT_URI} ]]
+then
+    echo "Service Account detected - downloading..."
+    curl -L -o ${SERVICE_ACCOUNT} ${SERVICE_ACCOUNT_URI}
+else
+    echo "ServiceAccount uri not set."
+fi
