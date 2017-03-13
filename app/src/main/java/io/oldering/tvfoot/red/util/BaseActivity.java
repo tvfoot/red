92014eb313367974d1a2a82ec0498b17f032384e
benoit <oldering@gmail.com>
2017-03-13 17:55:58 +0900

    circling ci

--- app/src/main/java/io/oldering/tvfoot/red/util/BaseActivity.java
+++ app/src/main/java/io/oldering/tvfoot/red/util/BaseActivity.java
@@ -3,7 +3,6 @@ package io.oldering.tvfoot.red.util;
 import android.support.v7.app.AppCompatActivity;
 import io.oldering.tvfoot.red.di.ActivityComponentFactory;
 import io.oldering.tvfoot.red.di.component.ActivityComponent;
-import io.oldering.tvfoot.red.util.BundleService;
 
 public abstract class BaseActivity extends AppCompatActivity {
   protected ActivityComponent activityComponent;
