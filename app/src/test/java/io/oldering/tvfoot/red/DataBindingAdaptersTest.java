9316da095e00d984ee3b79f4b2cc8f7d7b1e3a32
benoit <oldering@gmail.com>
2017-03-13 16:38:36 +0900

    renamed file

--- app/src/test/java/io/oldering/tvfoot/red/DataBindingAdaptersTest.java
+++ app/src/test/java/io/oldering/tvfoot/red/DataBindingAdaptersTest.java
@@ -2,18 +2,16 @@ package io.oldering.tvfoot.red;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
 import android.widget.ImageView;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(AndroidJUnit4.class) public class DataBindingAdaptersTest {
+public class DataBindingAdaptersTest {
+
   @Test public void setVisibilityGone() {
     View view = mock(View.class);
     DataBindingAdapters.setVisibility(view, false);
@@ -27,7 +25,7 @@ import static org.mockito.Mockito.when;
   }
 
   // Multie dex files define...
-  @Ignore @Test public void setImageResource() {
+  @Test public void setImageResource() {
     String resourceName = "b1";
     String packageName = "packageName";
 
