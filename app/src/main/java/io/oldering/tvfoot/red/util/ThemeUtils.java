84dddc7156da358774e744fd6f1efa696bf1021e
benoit <oldering@gmail.com>
2017-04-21 14:47:13 +0900

    Polished a bit the theme manipulation on launch
    Just for fun

--- app/src/main/java/io/oldering/tvfoot/red/util/ThemeUtils.java
+++ app/src/main/java/io/oldering/tvfoot/red/util/ThemeUtils.java
@@ -1,20 +1,25 @@
 package io.oldering.tvfoot.red.util;
 
 import android.content.Context;
-import android.support.annotation.UiThread;
 import android.util.TypedValue;
 import io.oldering.tvfoot.red.R;
 
 public final class ThemeUtils {
-  private static final TypedValue TYPED_VALUE = new TypedValue();
+  private static final ThreadLocal<TypedValue> TYPED_VALUE = new ThreadLocal<TypedValue>() {
+    @Override protected TypedValue initialValue() {
+      return new TypedValue();
+    }
+  };
 
   private ThemeUtils() {
   }
 
-  @UiThread public static void ensureRuntimeTheme(Context context) {
-    context.getTheme().resolveAttribute(R.attr.ltRuntimeTheme, TYPED_VALUE, true);
-    if (TYPED_VALUE.resourceId != 0) {
-      context.setTheme(TYPED_VALUE.resourceId);
+  public static void ensureRuntimeTheme(Context context) {
+    final TypedValue typedValue = TYPED_VALUE.get();
+    context.getTheme().resolveAttribute(R.attr.ltRuntimeTheme, typedValue, true);
+    if (typedValue.resourceId <= 0) {
+      throw new IllegalArgumentException("runtimeTheme not defined in the preview theme");
     }
+    context.setTheme(typedValue.resourceId);
   }
 }
