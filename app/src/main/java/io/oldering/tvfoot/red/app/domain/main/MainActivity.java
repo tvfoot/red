2cf6eb0c475cacbbd89fa7b0c768a9032037bea5
benoit <oldering@gmail.com>
2017-05-03 15:24:12 +0900

    removed unused imports

--- app/src/main/java/io/oldering/tvfoot/red/app/domain/main/MainActivity.java
+++ app/src/main/java/io/oldering/tvfoot/red/app/domain/main/MainActivity.java
@@ -1,9 +1,8 @@
 package io.oldering.tvfoot.red.app.domain.main;
 
 import android.os.Bundle;
-import com.google.firebase.analytics.FirebaseAnalytics;
-import io.oldering.tvfoot.red.app.common.flowcontroller.FlowController;
 import io.oldering.tvfoot.red.app.common.BaseActivity;
+import io.oldering.tvfoot.red.app.common.flowcontroller.FlowController;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 
