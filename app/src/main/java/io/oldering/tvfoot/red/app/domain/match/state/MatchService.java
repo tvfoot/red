57a6a323ba3e027a86612b00d0ba6e81a627dea5
benoit <oldering@gmail.com>
2017-05-07 21:39:09 +0900

    implemented read/write function for pref in service

--- app/src/main/java/io/oldering/tvfoot/red/app/domain/match/state/MatchService.java
+++ app/src/main/java/io/oldering/tvfoot/red/app/domain/match/state/MatchService.java
@@ -1,18 +1,32 @@
 package io.oldering.tvfoot.red.app.domain.match.state;
 
+import android.content.SharedPreferences;
 import io.oldering.tvfoot.red.api.TvfootService;
+import io.oldering.tvfoot.red.app.common.Notification;
 import io.oldering.tvfoot.red.app.data.entity.Match;
 import io.reactivex.Single;
 import javax.inject.Inject;
 
 public class MatchService {
+  private static final String NOTIFY_MATCH_START = "NOTIFY_MATCH_START";
   private final TvfootService tvfootService;
+  private final SharedPreferences preferences;
 
-  @Inject MatchService(TvfootService tvfootService) {
+  @Inject MatchService(TvfootService tvfootService, SharedPreferences preferences) {
     this.tvfootService = tvfootService;
+    this.preferences = preferences;
   }
 
   public Single<Match> loadMatch(String matchId) {
     return tvfootService.getMatch(matchId);
+  }
+
+  public Single<Notification> saveNotifyMatchStart(boolean notifyMatchStart) {
+    preferences.edit().putBoolean(NOTIFY_MATCH_START, notifyMatchStart).apply();
+    return Single.just(Notification.INSTANCE);
+  }
+
+  public Single<Boolean> loadNotifyMatchStart() {
+    Single.just(preferences.getBoolean(NOTIFY_MATCH_START, false));
   }
 }
--- app/src/main/java/io/oldering/tvfoot/red/app/injection/module/ServiceModule.java
+++ app/src/main/java/io/oldering/tvfoot/red/app/injection/module/ServiceModule.java
@@ -1,13 +1,21 @@
 package io.oldering.tvfoot.red.app.injection.module;
 
+import android.app.Application;
+import android.content.SharedPreferences;
 import dagger.Module;
 import dagger.Provides;
 import io.oldering.tvfoot.red.api.TvfootService;
 import javax.inject.Singleton;
 import retrofit2.Retrofit;
 
+import static android.content.Context.MODE_PRIVATE;
+
 @Module public class ServiceModule {
-  @Provides @Singleton TvfootService provideTvfootService(Retrofit retrofit) {
+  @Provides @Singleton static TvfootService provideTvfootService(Retrofit retrofit) {
     return retrofit.create(TvfootService.class);
+  }
+
+  @Provides static SharedPreferences provideSharedPreferences(Application context) {
+    return context.getSharedPreferences("Red", MODE_PRIVATE);
   }
 }
