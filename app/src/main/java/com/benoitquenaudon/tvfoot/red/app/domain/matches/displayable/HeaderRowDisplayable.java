59e0e5e34765207a7e7851fdf79b1a5e026e06af
benoit <oldering@gmail.com>
2017-06-13 16:23:53 +0900

    better formated dates for headerRow

--- app/src/main/java/com/benoitquenaudon/tvfoot/red/app/domain/matches/displayable/HeaderRowDisplayable.java
+++ app/src/main/java/com/benoitquenaudon/tvfoot/red/app/domain/matches/displayable/HeaderRowDisplayable.java
@@ -1,15 +1,24 @@
 package com.benoitquenaudon.tvfoot.red.app.domain.matches.displayable;
 
-import com.google.auto.value.AutoValue;
 import com.benoitquenaudon.tvfoot.red.R;
 import com.benoitquenaudon.tvfoot.red.util.DateUtils;
+import com.google.auto.value.AutoValue;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import timber.log.Timber;
 
-import static com.benoitquenaudon.tvfoot.red.app.common.TimeConstants.ONE_DAY_IN_MILLIS;
-
 @AutoValue public abstract class HeaderRowDisplayable implements MatchesItemDisplayable {
+  static SimpleDateFormat headerKeyDateFormat =
+      new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+  static SimpleDateFormat dayOfWeekDateFormat = new SimpleDateFormat("EEEE", Locale.getDefault());
+  static SimpleDateFormat dayDateFormat = new SimpleDateFormat("EEEE d", Locale.getDefault());
+  static SimpleDateFormat monthDateFormat =
+      new SimpleDateFormat("EEEE, dd MMMM", Locale.getDefault());
+  static SimpleDateFormat yearDateFormat =
+      new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
+
   public abstract int dangerResId();
 
   public abstract boolean hasDanger();
@@ -17,23 +26,37 @@ import static com.benoitquenaudon.tvfoot.red.app.common.TimeConstants.ONE_DAY_IN
   public abstract String displayedDate();
 
   public static HeaderRowDisplayable create(String headerKey) {
-    int dangerResId = -1;
-    String displayedDate;
-
     Date date;
     try {
-      date = MatchRowDisplayable.mediumDateFormat.parse(headerKey);
+      date = headerKeyDateFormat.parse(headerKey);
     } catch (ParseException e) {
       Timber.e(e);
       throw new UnsupportedOperationException("What is this date anyway? " + headerKey);
     }
+
+    int dangerResId = -1;
+    String displayedDate;
     if (DateUtils.isToday(date.getTime())) {
       dangerResId = R.string.matches_row_header_danger_today;
-    } else if (DateUtils.isToday(date.getTime() - ONE_DAY_IN_MILLIS)) {
-      dangerResId = R.string.matches_row_header_danger_tomorrow;
+      displayedDate = dayOfWeekDateFormat.format(date);
+      return new AutoValue_HeaderRowDisplayable(dangerResId, true, displayedDate);
     }
-    displayedDate = headerKey;
-    return new AutoValue_HeaderRowDisplayable(dangerResId, dangerResId > 0, displayedDate);
+
+    if (DateUtils.isTomorrow(date.getTime())) {
+      dangerResId = R.string.matches_row_header_danger_tomorrow;
+      displayedDate = dayOfWeekDateFormat.format(date);
+      return new AutoValue_HeaderRowDisplayable(dangerResId, true, displayedDate);
+    }
+
+    if (DateUtils.isThisWeek(date.getTime())) {
+      displayedDate = dayDateFormat.format(date);
+    } else if (DateUtils.isThisYear(date.getTime())) {
+      displayedDate = monthDateFormat.format(date);
+    } else {
+      displayedDate = yearDateFormat.format(date);
+    }
+
+    return new AutoValue_HeaderRowDisplayable(dangerResId, false, displayedDate);
   }
 
   @Override public boolean isSameAs(MatchesItemDisplayable newItem) {
--- app/src/main/java/com/benoitquenaudon/tvfoot/red/util/DateUtils.java
+++ app/src/main/java/com/benoitquenaudon/tvfoot/red/util/DateUtils.java
@@ -1,6 +1,14 @@
 package com.benoitquenaudon.tvfoot.red.util;
 
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.DAY_OF_WEEK;
+import static java.util.Calendar.MONDAY;
+import static java.util.Calendar.MONTH;
+import static java.util.Calendar.WEEK_OF_YEAR;
+import static java.util.Calendar.YEAR;
 
 public class DateUtils {
 
@@ -10,8 +18,33 @@ public class DateUtils {
     Calendar targetCalendar = Calendar.getInstance();
     targetCalendar.setTimeInMillis(time);
 
-    return nowCalendar.get(Calendar.YEAR) == targetCalendar.get(Calendar.YEAR)
-        && nowCalendar.get(Calendar.MONTH) == targetCalendar.get(Calendar.MONTH)
-        && nowCalendar.get(Calendar.DAY_OF_MONTH) == targetCalendar.get(Calendar.DAY_OF_MONTH);
+    return nowCalendar.get(YEAR) == targetCalendar.get(YEAR)
+        && nowCalendar.get(MONTH) == targetCalendar.get(MONTH)
+        && nowCalendar.get(DAY_OF_MONTH) == targetCalendar.get(DAY_OF_MONTH);
+  }
+
+  public static boolean isTomorrow(long time) {
+    return isToday(time - TimeUnit.DAYS.toMillis(1));
+  }
+
+  public static boolean isThisWeek(long time) {
+    Calendar nowCalendar = Calendar.getInstance();
+    nowCalendar.set(DAY_OF_WEEK, MONDAY);
+
+    Calendar targetCalendar = Calendar.getInstance();
+    targetCalendar.setTimeInMillis(time);
+    targetCalendar.set(DAY_OF_WEEK, MONDAY);
+
+    return nowCalendar.get(YEAR) == targetCalendar.get(YEAR)
+        && nowCalendar.get(WEEK_OF_YEAR) == targetCalendar.get(WEEK_OF_YEAR);
+  }
+
+  public static boolean isThisYear(long time) {
+    Calendar nowCalendar = Calendar.getInstance();
+
+    Calendar targetCalendar = Calendar.getInstance();
+    targetCalendar.setTimeInMillis(time);
+
+    return nowCalendar.get(YEAR) == targetCalendar.get(YEAR);
   }
 }
