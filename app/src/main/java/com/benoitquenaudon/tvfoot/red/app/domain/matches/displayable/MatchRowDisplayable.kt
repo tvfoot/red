7519530eafd63393efd3f4c8e9ee28be1c25486b
benoit <oldering@gmail.com>
2017-07-31 21:45:37 +0900

    match day can be null

--- app/src/main/java/com/benoitquenaudon/tvfoot/red/app/domain/matches/displayable/MatchRowDisplayable.kt
+++ app/src/main/java/com/benoitquenaudon/tvfoot/red/app/domain/matches/displayable/MatchRowDisplayable.kt
@@ -21,102 +21,101 @@ data class MatchRowDisplayable private constructor(
     val broadcasters: List<BroadcasterRowDisplayable>,
     val headline: String,
     val competition: String,
-    val matchDay: String,
+    val matchDay: String?,
     val live: Boolean,
     val homeTeamDrawableName: String,
     val awayTeamDrawableName: String,
     val location: String,
     val matchId: String
 ) : MatchesItemDisplayable {
-
   override fun isSameAs(newItem: MatchesItemDisplayable): Boolean {
     return newItem is MatchRowDisplayable && this.matchId == newItem.matchId
   }
 
   companion object Factory {
-    private val shortDateFormat: DateFormat by lazy(NONE) {
-      val format = SimpleDateFormat("HH:mm", Locale.getDefault())
-      format.timeZone = TimeZone.getDefault()
-      format
-    }
-    val mediumDateFormat: DateFormat by lazy(NONE) {
-      val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-      format.timeZone = TimeZone.getDefault()
-      format
-    }
-
-    fun fromMatch(match: Match): MatchRowDisplayable {
-      return MatchRowDisplayable(
-          parseHeaderKey(match.startAt()),
-          parseStartTime(match.startAt()),
-          parseBroadcasters(match.broadcasters()),
-          parseHeadLine(match.homeTeam(), match.awayTeam(), match.label()),
-          parseCompetition(match.competition()),
-          parseMatchDay(match.label(), match.matchDay()),
-          isMatchLive(match.startAt()),
-          parseHomeTeamDrawableName(match.homeTeam()),
-          parseAwayTeamDrawableName(match.awayTeam()),
-          parseLocation(match),
-          match.id())
-    }
+    fun fromMatch(match: Match): MatchRowDisplayable =
+        MatchRowDisplayable(
+            headerKey = parseHeaderKey(match.startAt()),
+            startTime = parseStartTime(match.startAt()),
+            broadcasters = parseBroadcasters(match.broadcasters()),
+            headline = parseHeadLine(match.homeTeam(), match.awayTeam(), match.label()),
+            competition = parseCompetition(match.competition()),
+            matchDay = parseMatchDay(match.label(), match.matchDay()),
+            live = isMatchLive(match.startAt()),
+            homeTeamDrawableName = parseHomeTeamDrawableName(match.homeTeam()),
+            awayTeamDrawableName = parseAwayTeamDrawableName(match.awayTeam()),
+            location = parseLocation(match),
+            matchId = match.id())
 
     fun fromMatches(matches: List<Match>): List<MatchRowDisplayable> = matches.map(this::fromMatch)
-
-    private fun parseHeaderKey(startAt: Date): String = mediumDateFormat.format(startAt)
-
-    private fun parseStartTime(startAt: Date): String = shortDateFormat.format(startAt)
-
-    private fun parseBroadcasters(
-        broadcasters: List<Broadcaster>?): List<BroadcasterRowDisplayable> {
-      if (broadcasters == null) {
-        return ArrayList()
-      }
-
-      return broadcasters.map { BroadcasterRowDisplayable.create(it.name(), it.code()) }
-    }
-
-    private fun parseHeadLine(homeTeam: Team, awayTeam: Team, matchLabel: String?): String {
-      if (homeTeam.name().isNullOrEmpty() || awayTeam.name().isNullOrEmpty()) {
-        return checkNotNull(matchLabel).toString()
-      }
-
-      return homeTeam.name().toString().toUpperCase() +
-          " - " +
-          awayTeam.name().toString().toUpperCase()
-    }
-
-    private fun parseCompetition(competition: Competition): String = competition.name()
-
-    private fun parseMatchDay(matchLabel: String?, matchDay: String?): String {
-      if (matchLabel.isNullOrEmpty()) {
-        if (matchDay.isNullOrEmpty()) throw IllegalStateException("matchDay is empty")
-
-        val dayAsInt: Int? = matchDay?.toIntOrNull()
-        if (dayAsInt == null) {
-          return checkNotNull(matchDay)
-        } else {
-          return "J. $matchDay"
-        }
-      } else {
-        return checkNotNull(matchLabel)
-      }
-    }
-
-    private fun isMatchLive(startAt: Date): Boolean {
-      val now = Calendar.getInstance().timeInMillis
-      val startTimeInMillis = startAt.time
-
-      return now in startTimeInMillis..(startTimeInMillis + TimeUnit.MINUTES.toMillis(105))
-    }
-
-    private fun parseHomeTeamDrawableName(homeTeam: Team): String {
-      return homeTeam.code() ?: Team.DEFAULT_CODE
-    }
-
-    private fun parseAwayTeamDrawableName(awayTeam: Team): String {
-      return awayTeam.code() ?: Team.DEFAULT_CODE
-    }
-
-    private fun parseLocation(match: Match): String = match.place().toString()
   }
 }
+
+private val shortDateFormat: DateFormat by lazy(NONE) {
+  val format = SimpleDateFormat("HH:mm", Locale.getDefault())
+  format.timeZone = TimeZone.getDefault()
+  format
+}
+val mediumDateFormat: DateFormat by lazy(NONE) {
+  val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+  format.timeZone = TimeZone.getDefault()
+  format
+}
+
+
+private fun parseHeaderKey(startAt: Date): String = mediumDateFormat.format(startAt)
+
+private fun parseStartTime(startAt: Date): String = shortDateFormat.format(startAt)
+
+private fun parseBroadcasters(
+    broadcasters: List<Broadcaster>?): List<BroadcasterRowDisplayable> {
+  if (broadcasters == null) {
+    return ArrayList()
+  }
+
+  return broadcasters.map { BroadcasterRowDisplayable.create(it.name(), it.code()) }
+}
+
+private fun parseHeadLine(homeTeam: Team, awayTeam: Team, matchLabel: String?): String {
+  if (homeTeam.name().isNullOrEmpty() || awayTeam.name().isNullOrEmpty()) {
+    return checkNotNull(matchLabel).toString()
+  }
+
+  return homeTeam.name().toString().toUpperCase() +
+      " - " +
+      awayTeam.name().toString().toUpperCase()
+}
+
+private fun parseCompetition(competition: Competition): String = competition.name()
+
+private fun parseMatchDay(matchLabel: String?, matchDay: String?): String? {
+  if (matchLabel.isNullOrEmpty()) {
+    if (matchDay.isNullOrEmpty()) return null // happens sometimes...
+
+    val dayAsInt: Int? = matchDay?.toIntOrNull()
+    if (dayAsInt == null) {
+      return checkNotNull(matchDay)
+    } else {
+      return "J. $matchDay"
+    }
+  } else {
+    return checkNotNull(matchLabel)
+  }
+}
+
+private fun isMatchLive(startAt: Date): Boolean {
+  val now = Calendar.getInstance().timeInMillis
+  val startTimeInMillis = startAt.time
+
+  return now in startTimeInMillis..(startTimeInMillis + TimeUnit.MINUTES.toMillis(105))
+}
+
+private fun parseHomeTeamDrawableName(homeTeam: Team): String {
+  return homeTeam.code() ?: Team.DEFAULT_CODE
+}
+
+private fun parseAwayTeamDrawableName(awayTeam: Team): String {
+  return awayTeam.code() ?: Team.DEFAULT_CODE
+}
+
+private fun parseLocation(match: Match): String = match.place().toString()
