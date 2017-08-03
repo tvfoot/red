396996278d4697be81d6142c00d71ada22901d4b
benoit <oldering@gmail.com>
2017-08-03 14:43:27 +0900

    Robolectric new config

--- app/src/test/java/com/benoitquenaudon/tvfoot/red/app/common/PreferenceRepositoryTest.kt
+++ app/src/test/java/com/benoitquenaudon/tvfoot/red/app/common/PreferenceRepositoryTest.kt
@@ -3,18 +3,14 @@ package com.benoitquenaudon.tvfoot.red.app.common
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
-import com.benoitquenaudon.tvfoot.red.BuildConfig
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
-import org.robolectric.annotation.Config
 import kotlin.properties.Delegates
 
 @RunWith(RobolectricTestRunner::class)
-@Config(constants = BuildConfig::class)
 class PreferenceRepositoryTest {
   private val SHARED_PREF = "ref_test"
   private var matchId by Delegates.notNull<String>()
@@ -25,15 +21,14 @@ class PreferenceRepositoryTest {
     preferenceService = PreferenceRepository(fakeSharedPreferences())
   }
 
-  @Ignore(
-      "RobolectricNotFound...") @Test fun loadNotifyMatchStart_shouldReturnDefaultValueWhenNone() {
+  @Test fun loadNotifyMatchStart_shouldReturnDefaultValueWhenNone() {
     val obs = preferenceService.loadNotifyMatchStart(matchId).test()
     obs.assertValueCount(1)
     obs.assertComplete()
     obs.assertValue(false)
   }
 
-  @Ignore("RobolectricNotFound...") @Test fun loadNotifyMatchStart_shouldReturnTrue() {
+  @Test fun loadNotifyMatchStart_shouldReturnTrue() {
     preferenceService.saveNotifyMatchStart(matchId, true)
 
     val obs = preferenceService.loadNotifyMatchStart(matchId).test()
@@ -42,7 +37,7 @@ class PreferenceRepositoryTest {
     obs.assertValue(true)
   }
 
-  @Ignore("RobolectricNotFound...") @Test fun loadNotifyMatchStart_shouldReturnFalse() {
+  @Test fun loadNotifyMatchStart_shouldReturnFalse() {
     preferenceService.saveNotifyMatchStart(matchId, false)
 
     val obs = preferenceService.loadNotifyMatchStart(matchId).test()
@@ -52,7 +47,7 @@ class PreferenceRepositoryTest {
   }
 
   // is that enough?
-  @Ignore("RobolectricNotFound...") @Test fun saveNotifyMatchStart() {
+  @Test fun saveNotifyMatchStart() {
     val obs = preferenceService.saveNotifyMatchStart(matchId, true).test()
     obs.assertValueCount(1)
     obs.assertComplete()
--- app/src/test/java/com/benoitquenaudon/tvfoot/red/app/common/flowcontroller/FlowIntentFactoryTest.kt
+++ app/src/test/java/com/benoitquenaudon/tvfoot/red/app/common/flowcontroller/FlowIntentFactoryTest.kt
@@ -4,29 +4,27 @@ import android.content.Intent
 import android.net.Uri
 import com.benoitquenaudon.tvfoot.red.BuildConfig
 import org.junit.Assert
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(constants = BuildConfig::class)
 class FlowIntentFactoryTest {
-  @Ignore("RobolectricNotFound...") @Test fun toMatches() {
+  @Test fun toMatches() {
     val intent = FlowIntentFactory.toMatchesIntent()
     Assert.assertEquals(Intent.ACTION_VIEW, intent.action)
     Assert.assertEquals(Uri.parse("tvfoot://tvfoot/"), intent.data)
   }
 
-  @Ignore("RobolectricNotFound...") @Test fun toMatch() {
+  @Test fun toMatch() {
     val matchId = "match_id"
     val intent = FlowIntentFactory.toMatchIntent(matchId)
     Assert.assertEquals(Intent.ACTION_VIEW, intent.action)
     Assert.assertEquals(Uri.parse("tvfoot://tvfoot/match/league/home/away/$matchId"), intent.data)
   }
 
-  @Ignore("RobolectricNotFound...") @Test fun toSettings() {
+  @Test fun toSettings() {
     val intent = FlowIntentFactory.toSettingsIntent()
     Assert.assertEquals(Intent.ACTION_VIEW, intent.action)
     Assert.assertEquals(Uri.parse("tvfoot://tvfoot/settings"), intent.data)
