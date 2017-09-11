73412357da5680650cb3ab24c9ec00ea37d2bafe
benoit <oldering@gmail.com>
2017-09-11 16:39:28 +0900

    Removed the header decoration

--- app/src/main/java/com/benoitquenaudon/tvfoot/red/app/domain/matches/MatchesActivity.kt
+++ app/src/main/java/com/benoitquenaudon/tvfoot/red/app/domain/matches/MatchesActivity.kt
@@ -57,7 +57,6 @@ class MatchesActivity : BaseActivity(), MviView<MatchesIntent, MatchesViewState>
   private fun setupView() {
     binding = DataBindingUtil.setContentView(this, R.layout.activity_matches)
     binding.recyclerView.adapter = adapter
-    binding.recyclerView.addItemDecoration(MatchesHeaderDecoration(adapter), 0)
     binding.viewModel = viewModel
 
     setSupportActionBar(binding.matchesToolbar)
