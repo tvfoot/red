1af124794f868fe8971a5a3273f3c3f47e720a19
benoit <oldering@gmail.com>
2017-10-12 15:18:10 +0900

    Removed mock flavor

--- app/src/test/java/com/benoitquenaudon/tvfoot/red/app/common/flowcontroller/FlowIntentFactoryTest.kt
+++ app/src/test/java/com/benoitquenaudon/tvfoot/red/app/common/flowcontroller/FlowIntentFactoryTest.kt
@@ -4,25 +4,25 @@ import android.content.Intent
 import android.net.Uri
 import org.junit.Assert
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
 class FlowIntentFactoryTest {
-  @Test fun toMatches() {
+  @Test
+  fun toMatches() {
     val intent = FlowIntentFactory.toMatchesIntent()
     Assert.assertEquals(Intent.ACTION_VIEW, intent.action)
     Assert.assertEquals(Uri.parse("tvfoot://tvfoot/"), intent.data)
   }
 
-  @Test fun toMatch() {
+  @Test
+  fun toMatch() {
     val matchId = "match_id"
     val intent = FlowIntentFactory.toMatchIntent(matchId)
     Assert.assertEquals(Intent.ACTION_VIEW, intent.action)
     Assert.assertEquals(Uri.parse("tvfoot://tvfoot/match/league/home/away/$matchId"), intent.data)
   }
 
-  @Test fun toSettings() {
+  @Test
+  fun toSettings() {
     val intent = FlowIntentFactory.toSettingsIntent()
     Assert.assertEquals(Intent.ACTION_VIEW, intent.action)
     Assert.assertEquals(Uri.parse("tvfoot://tvfoot/settings"), intent.data)
