93c59cd52df4a2e4f03ff9792653aece7194df87
benoit <oldering@gmail.com>
2017-12-12 15:27:36 +0900

    Allow team into search query

--- app/src/main/java/com/benoitquenaudon/tvfoot/red/app/data/entity/search/Deleted.kt
+++
@@ -1,5 +0,0 @@
-package com.benoitquenaudon.tvfoot.red.app.data.entity.search
-
-data class Deleted(private val neq: Int = 1) {
-  override fun toString() = """{"neq":$neq}"""
-}
--- app/src/main/java/com/benoitquenaudon/tvfoot/red/app/data/entity/search/Where.kt
+++ app/src/main/java/com/benoitquenaudon/tvfoot/red/app/data/entity/search/Where.kt
@@ -1,12 +1,29 @@
 package com.benoitquenaudon.tvfoot.red.app.data.entity.search
 
+import com.benoitquenaudon.tvfoot.red.util.TeamCode
 import com.squareup.moshi.formatIso8601
 import java.util.Date
 
 data class Where(
     private val startAt: Long = System.currentTimeMillis(),
-    private val deleted: Deleted = Deleted()
+    private val onlyBroadcasted: Boolean = false,
+    private val teams: List<TeamCondition> = emptyList()
 ) {
-  override fun toString() =
-      """{"start-at":{"gte":"${Date(startAt).formatIso8601()}"},"deleted":$deleted}"""
+  override fun toString(): String {
+    val buffer = mutableListOf<String>()
+
+    buffer.add(""""start-at":{"gte":"${Date(startAt).formatIso8601()}"}""")
+    buffer.add(""""deleted":{"neq":1}""")
+    if (onlyBroadcasted) buffer.add(""""broadcasters":{"gt":[]}""")
+    if (teams.isNotEmpty()) buffer.add(""""or":[${teams.joinToString(",")}]""")
+
+    return buffer.joinToString(prefix = "{", separator = ",", postfix = "}")
+  }
+
+  data class TeamCondition(
+      private val code: TeamCode
+  ) {
+    override fun toString() =
+        """{"home-team.code":"$code"},{"away-team.code":"$code"}"""
+  }
 }
